7b6cedeac02756a041878118e321cb3d0f83ac6a
Chathura Jayawardhana <105495683+ChathurJayawardhana@users.noreply.github.com>
2023-12-20 18:41:52 +0530

    hero

--- chathuraportfolio/src/pages/Home.js
+++ chathuraportfolio/src/pages/Home.js
@@ -1,63 +1,29 @@
 import React from 'react'
-import { Link } from 'react-scroll';
-import { MdKeyboardArrowRight } from "react-icons/md";
-import {Grid} from '@mui/material'
 import Hero from '../assets/thara.jpg'
 
-export default function Home() {
+export default function  () {
   return (
-    <div className='max-w-auto h-[600px] w-full py-4 px-4 relative'>
-    <Grid container spacing={4} justifyContent={'center'} >
-       <Grid item xs={12} sm={12} md={7} lg={8}>
-        <div className='pl-40 sm:px-8 sm:pr-10 md:pl-28'>
-        <h2 className='text-4xl sm:text-7xl  font-bold text-black'>
-                I am a Frontend Developer
-            </h2>
-            <p className='text-orange-800 py-4 max-w-md'>
-            "Hello, I'm  a frontend developer dedicated to 
+    <div className='w-full bg-white py-16 px-4'>
+      
+       
+       <div className='w-[1520] mx-auto grid md:grid-cols-2'>
+         <img className='w-[400px] rounded-full mx-auto my-4' src={Hero}/>
+         <div className='flex flex-col justify-center'>
+          <>
+       
+           <h1 className='md:text-4xl sm:text-3xl  text-6xl font-bold py-'>  I am a Frontend Developer</h1>
+           <p className='pt-8'>"Hello, I'm  a frontend developer dedicated to 
             crafting seamless, user-centric web experiences. 
             With expertise in HTML, CSS, JavaScript and ReactJS I specialize
-             in translating design concepts into elegant and responsive interfaces."
-            </p>
-        </div>
-        
+             in translating design concepts into elegant and responsive interfaces."</p>
+          </>
+          <button className='bg-black text-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto md:mx-0 py-3'>
+            Contact Me
+          </button>
+                 
+         </div>
+       </div>
       
-       </Grid>
-
-       <Grid item xs={12} sm={12} md={5} lg={4}>
-         <img src={Hero}  className='pr-4 w-[500px] h-[500px] rounded-full sm:px-4'/>
-       </Grid>
-
-
-    </Grid>
-
-
-
-    <Grid container>
-      <Grid item>
-      <div className='flex space-x-4 pl-40 pt-10'>
-                <div>
-                <Link
-              to="contact"
-              smooth
-              duration={500}
-              className="group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer"
-            >
-              Contact Me
-              <span className="group-hover:rotate-90 duration-300">
-                < MdKeyboardArrowRight size={25} className="ml-1" />
-              </span>
-            </Link>
-                </div>
-               
-                
-            </div>
-      </Grid>
-
-    </Grid>
-
     </div>
-      
-    
   )
 }
