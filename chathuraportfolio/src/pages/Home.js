48195117b537a848836b6567f9b10101936d1d99
Chathura Jayawardhana <105495683+ChathurJayawardhana@users.noreply.github.com>
2023-12-22 22:04:32 +0530

    home

--- chathuraportfolio/src/pages/Home.js
+++ chathuraportfolio/src/pages/Home.js
@@ -1,18 +1,26 @@
 import React from 'react'
 import Hero from '../assets/thara.jpg'
+import bgimage from '../assets/backgroundport.jpg'
 
 export default function  () {
+  const sectionStyle = {
+    backgroundImage: `url(${bgimage})`,
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    filter: 'brightness(70%)', 
+  };
+  
   return (
-    <div name="home" className='w-full bg-white py-16 px-4'>
+    <div name="home" className='w-full  py-16 px-4 ' style={sectionStyle}>
       
        
-       <div className='w-[1520] mx-auto grid md:grid-cols-2'>
+       <div className='w-[1520] mx-auto grid md:grid-cols-2 ' >
          <img className='w-[300px] rounded-full mx-auto my-4' src={Hero}/>
          <div className='flex flex-col justify-center'>
           <>
        
-           <h1 className=' md:text-4xl sm:text-3xl lg:text-6xl font-bold py-'>Hi,I'm Chathura Jayawardhana</h1>
-           <p className='pt-8 md:text-3xl sm:text-2xl lg:text-4xl text-orange-800'>"Front-End & MERN Stack Developer"</p>
+           <h1 className='sm:text-6xl md:text-5xl  lg:text-4xl font-bold text-white'>Hi,I'm  <span className='text-orange-700'>Chathura Jayawardhana</span></h1>
+           <p className='pt-8 md:text-4xl sm:text-4xl lg:text-3xl text-white'>"Front-End & MERN Stack Developer"</p>
           </>
           <button className='bg-gradient-to-r from-cyan-500 to-blue-500 text-white font-bold w-[200px] rounded-md  my-6 mx-auto md:mx-0 py-3'>
             Contact Me
