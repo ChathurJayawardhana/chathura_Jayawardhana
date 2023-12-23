acd848136c0b1065a1bb85a5538d5f5adb71e639
Chathura Jayawardhana <105495683+ChathurJayawardhana@users.noreply.github.com>
2023-12-23 23:51:39 +0530

    ccc

--- chathuraportfolio/src/components/Technical.js
+++ chathuraportfolio/src/components/Technical.js
@@ -88,7 +88,7 @@ const Experience = () => {
       name="experience"
       className="bg-white w-full h-screen"
     >
-      <div className="max-w-screen-lg mx-auto p-4 flex flex-col justify-center w-full h-full text-black ">
+      <div className="max-w-screen-lg mx-auto p-4 flex flex-col justify-center w-full h-full text-black pt-6 ">
       <h3 className='text-green-700 font-bold text-2xl px-4 pt-2'><u>Technical Skils</u></h3>
 
         <div className="w-full grid grid-cols-3 sm:grid-cols-4 lg:grid-cols-4 gap-8 text-center py-8 px-12 sm:px-0">
@@ -98,7 +98,7 @@ const Experience = () => {
               className={`shadow-md hover:scale-105 duration-500 py-2 rounded-lg ${style}`}
             >
               <img src={src} alt="" className="w-20 mx-auto" />
-              <p className="mt-4">{title}</p>
+              <p className="mt-4 font-bold md:text-xl lg:text-2xl hidden sm:block">{title}</p>
             </div>
           ))}
         </div>
--- chathuraportfolio/src/pages/About.js
+++ chathuraportfolio/src/pages/About.js
@@ -6,7 +6,7 @@ import Testimonials from '../components/Testimonials';
 export default function About() {
   return (
     <div name="about" className='w-full bg-white py-16 px-8'>
-      <h3 className='text-orange-500 font-bold text-2xl text-center'>Learn More About Me</h3>
+      <h3 className='text-orange-500 font-bold text-2xl text-center'> About Me</h3>
       <div className='w-[1520] mx-auto pt-6 flex flex-col items-center'>
         <div className='pt-4'>
           <div className='bg-gray-500 px-3 py-3 rounded-full'>
@@ -15,8 +15,8 @@ export default function About() {
         
         </div>
 
-        <div className='flex flex-col justify-center pt-8 md:text-4xl sm:text-3xl lg:text-3xl text-black'>
-          <p className='px-8'>
+        <div className='flex flex-col justify-center pt-8 md:text-3xl sm:text-2xl lg:text-3xl text-black'>
+          <p className='px-8 text-center'>
             I work as a <b>Frontend</b> and <b>React </b> developer and have worked with data analysts using
             <b>PowerBI</b> and as well as <b>Figma</b> design. I enjoy hearing things out. I've gained a lot of knowledge
             by paying close attention. I've discovered how to combine work and life. I am a very content individual. I
