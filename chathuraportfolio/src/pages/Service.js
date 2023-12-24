4192485486831c9581c0dd86c9b0042e377e8a8e
Chathura Jayawardhana <105495683+ChathurJayawardhana@users.noreply.github.com>
2023-12-24 15:30:39 +0530

    Revert "service sec"
    This reverts commit 8b357827d4a1fb29d6add923dd8dd78395a8a587.

--- chathuraportfolio/src/pages/Service.js
+++ chathuraportfolio/src/pages/Service.js
@@ -1,63 +1,60 @@
-import React from 'react'
-import react from '../assets/skills/react.png';
-import figma from '../assets/skills/Figma.png';
-import network from '../assets/skills/network.jpg';
-import power from '../assets/skills/PowerBi.jpg';
-
-
-
+import React from 'react';
+import { FaFigma, FaReact, FaWordpress, FaNetworkWired } from 'react-icons/fa';
+import { SiPowerbi } from 'react-icons/si';
 
 export default function Service() {
 
 const service = [
     {
-        id:1,
-        src:react,
-        Title:"Web Development",
-        description:"I'll Develop all Kind Of Web Development Using React and Mern Stack projects"
+      id: 1,
+      icon: <FaReact />,
+      title: 'Web Development',
+      description: "I'll Develop all Kind Of Web Development Using React and Mern Stack projects",
     },
     {
-        id:2,
-        src:figma,
-        Title:"UI-UX Designe",
-        description:"I'll Designe all of Web interfaces using Figma"
-    },
-      
-    {
-        id:4,
-        src:network,
-        Title:"Network TroubleShooting",
-        description:"I'll solving all Network Problems "
+      id: 2,
+      icon: <FaFigma />,
+      title: 'UI-UX Designe',
+      description: "I'll Designe all of Web interfaces using Figma",
     },
     {
-        id:5,
-        src:power,
-        Title:"Data Analyst",
-        description:"I'll Analyst all data analyst Project Using   "
+      id: 3,
+      icon: <FaWordpress />,
+      title: 'WordPress Developing',
+      description: "I'll develop all kinds of Wordpress websites and store with premium design",
+    },
+    {
+      id: 4,
+      icon: <FaNetworkWired />,
+      title: 'Network TroubleShooting',
+      description: "I'll solving all Network Problems ",
+    },
+    {
+      id: 5,
+      icon: <SiPowerbi />,
+      title: 'Data Analyst',
+      description: "I'll Analyst all data analyst Project Using ",
     },
     
 ]
 
   return (
-    <div name="service" className='w-full bg-white py-16 px-8'>
-       <h3 className='text-orange-500 font-bold text-2xl text-center'>Services</h3>
-       <div className='grid md:grid-cols-2 sm:grid-cols-1 lg:grid-cols-3 gap-6 pt-6 py-4 px-4 pl-12 shadow-md shadow-gray-600 rounded-lg'>
-        {
-            service.map((item)=>(
-                <div className='border-none hover:scale-105 hover:bg-gradient-to-r from-cyan-500 to-blue-500 duration-300  text-black/60 py-4 px-4'>
-               <div className='flex items-center pl-20'>
-               <img src={item.src} className='w-[75px] h-[75px] '/> 
-                </div>
-                
-                <div>
-                    <h3 className='pt-6 px-10 text-2xl font-bold'>{item.Title}</h3>
-                    <p className='pt-6 px-10  font-bold'>{item.description}</p>
-                </div>
-              </div>
-            ))
-        }
-       
-       </div>
+    <div name="service" className="w-full bg-white py-16 px-8">
+      <h3 className="text-orange-500 font-bold text-2xl text-center">Services</h3>
+      <div className="grid md:grid-cols-2 sm:grid-cols-1 lg:grid-cols-3 gap-6 py-4 px-4 pl-12">
+        {service.map((item) => (
+          <div
+            key={item.id}
+            className="border-none hover:scale-105 duration-300 bg-gradient-to-r from-cyan-500 to-blue-500 text-white py-4 px-4"
+          >
+            {item.icon}
+            <div>
+              <h3>{item.title}</h3>
+              <p>{item.description}</p>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
